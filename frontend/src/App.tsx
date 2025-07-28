9c208acfdc5066d63800029e03766bf4858b62cd
Ryan <ryan.a.ellis.1@gmail.com>
2025-07-28 18:56:06 -0400

    Update dependencies in frontend
    - Move "@types/react-dom" from dependencies to devDependencies
    - Update "typescript" version to "^5.8.3"
    - Add new devDependencies: "ajv", "ajv-keywords", "react-scripts"

---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const App: React.FC = () => {
+  return (
+    <div>
+      <h1>Resume Tailor</h1>
+      <p>Welcome to the Resume Tailor application!</p>
+    </div>
+  );
+};
+
+export default App;
